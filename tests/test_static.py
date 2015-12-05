8a88840cf292e28fbb4053014fe374fce122c23d
Matthew Perry <perrygeo@gmail.com>
2015-12-05 07:31:11 -0400

    I hate to do this for the sake of py26 but...

--- tests/test_static.py
+++ tests/test_static.py
@@ -40,6 +40,15 @@ def test_cli_static_features_stdin():
         status=200,
         content_type='image/png')
 
+    # alternate ordering for py26
+    responses.add(
+        responses.GET,
+        'https://api.mapbox.com/v4/mapbox.satellite/geojson(%7B%22features%22%3A%5B%7B%22geometry%22%3A%7B%22coordinates%22%3A%5B-122.7282%2C45.580100000000002%5D%2C%22type%22%3A%22Point%22%7D%2C%22id%22%3A%220%22%2C%22properties%22%3A%7B%7D%2C%22type%22%3A%22Feature%22%7D%2C%7B%22geometry%22%3A%7B%22coordinates%22%3A%5B-121.31529999999999%2C44.058199999999999%5D%2C%22type%22%3A%22Point%22%7D%2C%22id%22%3A%221%22%2C%22properties%22%3A%7B%7D%2C%22type%22%3A%22Feature%22%7D%5D%2C%22type%22%3A%22FeatureCollection%22%7D)/-61.7,12.1,12/600x600.png256?access_token=bogus',
+        match_querystring=True,
+        body='.PNG...',
+        status=200,
+        content_type='image/png')
+
     with open('tests/twopoints_seq.geojson', 'r') as src:
         stdin = src.read()
 
