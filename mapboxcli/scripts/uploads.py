ab05dc292d5f18037ae832ad2650108fee7a596b
Matthew Perry <perrygeo@gmail.com>
2016-03-08 09:08:39 -0400

    implement changes to upload interface

--- mapboxcli/scripts/uploads.py
+++ mapboxcli/scripts/uploads.py
@@ -5,17 +5,25 @@ from mapboxcli.errors import MapboxCLIException
 
 
 @click.command(short_help="Upload datasets to Mapbox accounts")
-@click.argument('tileset', required=True)
-@click.argument('infile', type=click.File('r'), required=True)
+@click.argument('args', nargs=-1, required=True, metavar="[INFILE] TILESET")
 @click.option('--name', default=None, help="Name for the data upload")
 @click.pass_context
-def upload(ctx, tileset, infile, name):
+def upload(ctx, args, name):
     """Upload data to Mapbox accounts.
     All endpoints require authentication.
     Uploaded data lands at https://www.mapbox.com/data/
     and can be used in new or existing projects.
 
-      $ mapbox upload username.data data.geojson
+    You can specify the input file and tileset id
+
+      $ mapbox upload mydata.geojson username.data
+
+    Or specify just the tileset id and take an input file on stdin
+
+      $ cat mydata.geojson | mapbox upload username.data
+
+    The --name option defines the title as it appears in Studio
+    and defaults to the last part of the tileset id, e.g. "data"
 
     Note that the tileset must start with your username.
     An access token with upload scope is required, see `mapbox --help`.
@@ -23,6 +31,22 @@ def upload(ctx, tileset, infile, name):
     access_token = (ctx.obj and ctx.obj.get('access_token')) or None
 
     service = mapbox.Uploader(access_token=access_token)
+
+    if len(args) == 1:
+        # Tileset specified, file from stdin
+        click.echo("Reading data from stdin (Hit Ctl-C to cancel) ...", err=True)
+        infile = click.File("rb")("-")
+        tileset = args[0]
+    elif len(args) == 2:
+        # Infile and Tileset are specified
+        infile = click.File("rb")(args[0])
+        tileset = args[1]
+    else:
+        raise click.UsageError(
+            "Must supply either 1 argument (tileset) or 2 (infile tileset)")
+
+    if name is None:
+        name = tileset.split(".")[-1]
 
     try:
         res = service.upload(infile, tileset, name)
