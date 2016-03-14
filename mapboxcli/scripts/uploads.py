2ecd8432e2ae24e714f023f61c35b6cb17972185
Matthew Perry <perrygeo@gmail.com>
2016-03-14 16:06:50 -0400

    revert back to 2 required args, ditch stdin for now

--- mapboxcli/scripts/uploads.py
+++ mapboxcli/scripts/uploads.py
@@ -5,10 +5,11 @@ from mapboxcli.errors import MapboxCLIException
 
 
 @click.command(short_help="Upload datasets to Mapbox accounts")
-@click.argument('args', nargs=-1, required=True, metavar="[INFILE] TILESET")
+@click.argument('infile', type=click.File('r'), required=True)
+@click.argument('tileset', required=True)
 @click.option('--name', default=None, help="Name for the data upload")
 @click.pass_context
-def upload(ctx, args, name):
+def upload(ctx, infile, tileset, name):
     """Upload data to Mapbox accounts.
     All endpoints require authentication.
     Uploaded data lands at https://www.mapbox.com/data/
@@ -31,25 +32,6 @@ def upload(ctx, args, name):
     access_token = (ctx.obj and ctx.obj.get('access_token')) or None
 
     service = mapbox.Uploader(access_token=access_token)
-
-    if len(args) == 1:
-        # Tileset specified, file from stdin
-        click.echo("Reading data from stdin (Hit Ctl-C to cancel) ...", err=True)
-        infile = click.File("rb")("-")
-        tileset = args[0]
-    elif len(args) == 2:
-        # Infile and Tileset are specified
-        try:
-            infile = click.File("rb")(args[0])
-        except click.ClickException:
-            raise click.UsageError(
-                "Could not open file: {0} "
-                "(check order of command arguments: INFILE TILESET)".format(args[0]))
-
-        tileset = args[1]
-    else:
-        raise click.UsageError(
-            "Must provide either one argument (TILESET) or two (INFILE TILESET)")
 
     if name is None:
         name = tileset.split(".")[-1]
