7b192e0b8cff72343ee314c7635b93ac42bbbd28
Matthew Perry <perrygeo@gmail.com>
2016-06-14 12:14:18 -0400

    0.4 release, resolves #74

--- mapboxcli/__init__.py
+++ mapboxcli/__init__.py
@@ -1,3 +1,3 @@
 # Command line interface to Mapbox Web Services
 
-__version__ = '0.3.1'
+__version__ = '0.4.0'
--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(name='mapboxcli',
           'click',
           'click-plugins',
           'cligj>=0.4',
-          'mapbox>=0.7',
+          'mapbox>=0.9',
           'six'
       ],
       extras_require={
