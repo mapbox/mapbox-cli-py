48a1a6dece3fc493edec943d4b52b0b5b42fe08c
Matthew Perry <perrygeo@gmail.com>
2016-11-22 11:54:05 -0400

    release 0.5.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(name='mapboxcli',
           'click',
           'click-plugins',
           'cligj>=0.4',
-          'mapbox>=0.9',
+          'mapbox>=0.10',
           'six'
       ],
       extras_require={
