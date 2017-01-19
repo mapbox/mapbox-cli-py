a59b9bc5100f7501da04cbe093c116b3a71f573e
Sean Gillies <sean@mapbox.com>
2017-01-19 18:23:04 +0100

    Fix help text

--- mapboxcli/scripts/uploads.py
+++ mapboxcli/scripts/uploads.py
@@ -12,25 +12,26 @@ from mapboxcli.errors import MapboxCLIException
 @click.pass_context
 def upload(ctx, tileset, source, name, patch):
     """Upload data to Mapbox accounts.
-    All endpoints require authentication.
-    Uploaded data lands at https://www.mapbox.com/data/
-    and can be used in new or existing projects.
 
-    You can specify the input file and tileset id
+    Uploaded data lands at https://www.mapbox.com/data/ and can be used
+    in new or existing projects. All endpoints require authentication.
 
-      $ mapbox upload mydata.geojson username.data
+    You can specify the tileset id and input file
+
+      $ mapbox upload username.data mydata.geojson
 
     Or specify just the tileset id and take an input file on stdin
 
       $ cat mydata.geojson | mapbox upload username.data
 
-    The --name option defines the title as it appears in Studio
-    and defaults to the last part of the tileset id, e.g. "data"
+    The --name option defines the title as it appears in Studio and
+    defaults to the last part of the tileset id, e.g. "data"
 
-    Note that the tileset must start with your username.
-    An access token with upload scope is required, see `mapbox --help`.
+    Note that the tileset must start with your username. An access
+    token with upload scope is required, see `mapbox --help`.
 
-    Your account must be flagged in order to use the patch mode feature.
+    Your account must be flagged in order to use the patch mode
+    feature.
     """
     access_token = (ctx.obj and ctx.obj.get('access_token')) or None
 
