627fff43c26bc96252ff63815b160f740f8a1657
critical-path <37241479+critical-path@users.noreply.github.com>
2018-04-15 21:53:50 -0400

    Add files via upload

---
+++ mapboxcli/scripts/analytics.py
@@ -0,0 +1,90 @@
+import click
+from json import dumps
+import mapbox
+from mapboxcli.errors import MapboxCLIException
+
+@click.command(
+  short_help="Returns the counts per day for a given resource and period."
+)
+
+@click.option(
+  "--resource-type", 
+  "-t", 
+  type=click.Choice(mapbox.Analytics.valid_resource_types), 
+  required=True,
+  help="The resource being requested."
+)
+
+@click.option(
+  "--username", 
+  "-u", 
+  type=str, 
+  required=True,
+  help="The username for the account that own the resource."
+)
+
+@click.option(
+  "--id", 
+  "-i", 
+  type=str,
+  help="The id for the resource."
+)
+
+@click.option(
+  "--start", 
+  "-s", 
+  type=str,
+  help="The ISO-formatted start date."
+)
+
+@click.option(
+  "--end", 
+  "-e", 
+  type=str,
+  help="The ISO-formatted end date."
+)
+
+@click.pass_context
+
+def analytics(ctx, resource_type, username, id, start, end):
+  """The Mapbox Analytics API returns the counts per day for
+     a given resource and period.
+
+
+     Long options:
+
+     $ mapbox analytics --resource-type <resource type> --username <username> --id <resource id> --start <ISO-formatted start date> --end <ISO-formatted end date>
+
+     Short options:
+
+     $ mapbox analytics -t <resource type> -u <username> -i <resource id> -s <ISO-formatted start date> -e <ISO-formatted end date>
+
+
+     An access token is required.  See "mapbox --help".
+  """
+
+  access_token = (ctx.obj and ctx.obj.get("access_token")) or None
+
+  service = mapbox.Analytics(access_token=access_token)
+
+  try:
+    response = service.analytics(
+      resource_type, 
+      username, 
+      id=id, 
+      start=start, 
+      end=end
+    )
+  except:
+    raise
+
+  if response.status_code == 200:
+    response_body = response.json()
+    response_body = dumps(response_body)
+    click.echo(response_body)
+  else:
+    response_body = response.text
+    response_body = response_body.strip()
+    raise MapboxCLIException(response_body)
+
+
