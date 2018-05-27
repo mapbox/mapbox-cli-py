0ba5b882dfa602cc72dacb4ce0bbc2100832ae15
critical-path <37241479+critical-path@users.noreply.github.com>
2018-05-27 15:10:53 -0400

    Update directions.py

--- mapboxcli/scripts/directions.py
+++ mapboxcli/scripts/directions.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 import click
 import cligj
@@ -6,6 +7,84 @@ import cligj
 import mapbox
 from mapboxcli.errors import MapboxCLIException
 
+
+def waypoint_snapping_callback(ctx, param, value):
+    results = []
+
+    list_pattern = re.compile("[,]")
+    int_pattern = re.compile("[0-9]")
+
+    # value is an n-tuple, each element of
+    # which contains input from the user.
+    #
+    # Iterate over each element, determining
+    # whether to convert it to a tuple,
+    # convert it to an int, or leave it as
+    # a str.
+    #
+    # Append each element to results, which
+    # the Directions SDK will attempt to
+    # validate.
+
+    if len(value) == 0:
+        return None
+
+    for element in value:
+
+        # If the element contains a comma, then assume 
+        # that the user intended to pass in a tuple.
+        #
+        # Convert each item in the element to an int,
+        # and create a tuple containing all items.
+        #
+        # Raise an error if the item is not a valid int.
+        #
+        # (The SDK accepts a three-tuple with ints for 
+        # radius, angle, and range.)
+
+        if re.search(list_pattern, element):
+            element = re.split(list_pattern, element)
+
+            for index in range(0, len(element)):
+                try:
+                    element[index] = int(element[index])
+                except ValueError as exc:
+                    raise mapbox.errors.ValidationError(str(exc))
+
+            element = tuple(element)
+
+            results.append(element)
+
+        # If the element contains a decimal number but not 
+        # a comma, then assume that the user intended to 
+        # pass in an int.
+        #
+        # Convert the element to an int.
+        #
+        # Raise an error if the item is not a valid int.
+        #
+        # (The Directions SDK accepts an int for radius.)
+
+        elif re.search(int_pattern, element):
+            try:
+                element = int(element)
+            except ValueError as exc:
+                raise mapbox.errors.ValidationError(str(exc))
+      
+            results.append(element)
+
+        # If the element contains neither a decimal number
+        # nor a comma, then assume that the user intended 
+        # to pass in a str.
+        #
+        # Do nothing since the element is aready a str.
+        #
+        # (The Directions SDK accepts a str for unlimited radius.)
+
+        else:
+            results.append(element)
+
+    return results
 
 @click.command(short_help="Routing between waypoints")
 
@@ -31,9 +110,10 @@ from mapboxcli.errors import MapboxCLIException
     help="Format of returned geometry"
 )
 
-# Directions.valid_geom_overview contains two items of type str 
-# and one item of type bool.  This causes the Directions CLI's 
-# --help option to raise a TypeError.  To prevent this, we convert
+# Directions.valid_geom_overview contains two 
+# elements of type str and one element of type bool.  
+# This causes the Directions CLI's --help option to 
+# raise a TypeError.  To prevent this, we convert
 # the bool to a str.
 
 @click.option(
@@ -52,6 +132,13 @@ from mapboxcli.errors import MapboxCLIException
     "--continue-straight/--no-continue-straight",
     default=True,
     help="Whether to see the allowed direction of travel when departing the original waypoint"
+)
+
+@click.option(
+    "--waypoint-snapping",
+    multiple=True,
+    callback=waypoint_snapping_callback,
+    help="Controls waypoint snapping"
 )
 
 @click.option(
@@ -74,7 +161,7 @@ from mapboxcli.errors import MapboxCLIException
 @click.pass_context
 def directions(ctx, features, profile, alternatives, 
                geometries, overview, steps, continue_straight, 
-               annotations, language, output):
+               waypoint_snapping, annotations, language, output):
     """The Mapbox Directions API will show you how to get
        where you're going.
 
@@ -87,8 +174,18 @@ def directions(ctx, features, profile, alternatives,
 
     service = mapbox.Directions(access_token=access_token)
 
+    # The Directions SDK expects False to be
+    # a bool, not a str.
+
     if overview == "False":
         overview = False
+
+    # When using waypoint snapping, the 
+    # Directions SDK expects features to be 
+    # a list, not a generator.
+
+    if waypoint_snapping is not None:
+        features = list(features)
 
     if annotations:
         annotations = annotations.split(",")
@@ -104,6 +201,7 @@ def directions(ctx, features, profile, alternatives,
             overview=overview,
             steps=steps,
             continue_straight=continue_straight,
+            waypoint_snapping=waypoint_snapping,
             annotations=annotations,
             language=language
         )
