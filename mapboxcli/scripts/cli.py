8084745eb6978a0dc002b7690e4bd9d21a07c1a5
critical-path <37241479+critical-path@users.noreply.github.com>
2018-06-01 21:24:52 -0400

    Update cli.py

--- mapboxcli/scripts/cli.py
+++ mapboxcli/scripts/cli.py
@@ -12,8 +12,15 @@ import cligj
 import mapboxcli
 from mapboxcli.compat import configparser
 from mapboxcli.scripts import (
-    config, geocoding, directions, distance, mapmatching, uploads, static,
-    surface, datasets, analytics)
+    config, 
+    geocoding, 
+    directions,
+    mapmatching, 
+    uploads, 
+    static,
+    datasets, 
+    analytics
+)
 
 
 def configure_logging(verbosity):
@@ -99,11 +106,8 @@ def main_group(ctx, verbose, quiet, access_token, config):
 main_group.add_command(config.config)
 main_group.add_command(geocoding.geocoding)
 main_group.add_command(directions.directions)
-main_group.add_command(distance.distance)
 main_group.add_command(mapmatching.match)
 main_group.add_command(uploads.upload)
 main_group.add_command(static.staticmap)
-main_group.add_command(surface.surface)
 main_group.add_command(datasets.datasets)
 main_group.add_command(analytics.analytics)
-
